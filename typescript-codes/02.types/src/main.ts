5a4f38d0b37d4586540bca146a6d3bd430922bbd
Max96312 <k0mp4ny16@gmail.com>
2023-11-09 23:20:55 +0900

    11/09 create typescript

---
+++ typescript-codes/02.types/src/main.ts
@@ -0,0 +1,9 @@
+let boolean: boolean
+let falseBoolean: boolean = false;
+
+let number: number
+let integer: number = 6
+let float: number = 1.2345
+
+let string: string
+let firstName:string = 'Doe';
