be610f22c1abf042315df687443f6594f7d94163
Max96312 <k0mp4ny16@gmail.com>
2023-11-30 19:47:05 +0900

    sec02 spring-basic

---
+++ Spring/Spring-Basic/demo/src/main/java/com/example/demo/OdderApp.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import com.example.demo.Order.Order;
+import com.example.demo.Order.OrderService;
+import com.example.demo.Order.OrderServiceImpl;
+import com.example.demo.member.Grade;
+import com.example.demo.member.Member;
+import com.example.demo.member.MemberService;
+import com.example.demo.member.MemberServiceImpl;
+
+public class OdderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice() = " + order.calculatePrice());
+
+    }
+}
